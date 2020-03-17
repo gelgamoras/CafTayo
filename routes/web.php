b888a8c4879c337d2ab3d04c6b3f3a4c71703794
jellily6 <gelgamoras@gmail.com>
2020-03-17 23:53:17 +0800

    fixed routes and route names

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,18 @@
 
 Route::get('/', 'MenuController@show_todaymenu')->name('menu_today'); 
 
-Route::get('/plan-menu', 'MenuController@create')->name('menu_create'); 
+Route::get('/menu/create', 'MenuController@create')->name('menu_create'); 
 
-Route::get('/view-menu', 'MenuController@show')->name('menu_view'); 
+Route::get('/menu/view', 'MenuController@show')->name('menu_view'); 
 
-Route::get('/manage-food', 'FoodController@index')->name('menu_list'); 
+Route::get('/food', 'FoodController@index')->name('menu_list'); 
 
-Route::get('/view-food', 'FoodController@show')->name('food_view'); 
+Route::get('/food/view', 'FoodController@show')->name('food_view'); 
 
-Route::get('/manage-menu', 'MenuController@index')->name('food_list'); 
+Route::get('/menu', 'MenuController@index')->name('food_list'); 
 
-Route::get('/everyday', 'MenuController@show_everydaymenu')->name('menu_everyday'); 
+Route::get('/menu/everyday', 'MenuController@show_everydaymenu')->name('menu_everyday'); 
 
 Route::get('/settings', 'PeriodController@get')->name('period_settings'); 
 
-Route::get('/new-food', 'FoodController@create')->name('food_create'); 
+Route::get('/food/create', 'FoodController@create')->name('food_create'); 
