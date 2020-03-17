b2a07e22e5c5b423daa3bbea7852887dacde5c32
jellily6 <gelgamoras@gmail.com>
2020-03-17 19:15:01 +0800

    transferred admin layout

---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer d-flex p-2 px-3 bg-white border-top">
+            
+            <span class="copyright ml-auto my-auto mr-2">Copyright © 2020
+              <a href="https://designrevision.com" rel="nofollow">GRR!</a>
+            </span>
+          </footer>
