6672eef1a382ea36c655574071d33211da3c8980
jellily6 <gelgamoras@gmail.com>
2020-03-22 23:34:50 +0800

    Oops wrong js folder, moved to the right one

---
+++ resources/views/layouts/concessionaire/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- 
+I'mma keep layouts for admin and concessionaire separate muna habang wala pang log in/users, 
+saka ko na ipagsama if meron na. Sorry still learning ✌️
+-->
+<footer class="main-footer d-flex p-2 px-3 bg-white border-top">
+  <span class="copyright ml-auto my-auto mr-2">Copyright © 2020
+    <a href="https://designrevision.com" rel="nofollow">GRR!</a>
+  </span>
+</footer>
---
+++ resources/views/layouts/concessionaire/topnav.blade.php
@@ -0,0 +1,39 @@
+<!-- 
+I'mma keep layouts for admin and concessionaire separate muna habang wala pang log in/users, 
+saka ko na ipagsama if meron na. Sorry still learning ✌️
+-->
+<nav class="navbar align-items-stretch justify-content-end navbar-light flex-md-nowrap p-0">
+  <!-- <form action="#" class="main-navbar__search w-100 d-none d-md-flex d-lg-flex">
+    <div class="input-group input-group-seamless ml-3">
+      <div class="input-group-prepend">
+        <div class="input-group-text">
+          <i class="fas fa-search"></i>
+        </div>
+      </div>
+      <input class="navbar-search form-control" type="text" placeholder="Search for something..." aria-label="Search"> </div>
+  </form> --> 
+  <ul class="navbar-nav border-left flex-row ">
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle text-nowrap px-3" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+        <img class="user-avatar rounded-circle mr-2" src="{{ asset('assets/images/avatars/0.jpg') }}" alt="User Avatar">
+        <span class="d-none d-md-inline-block">Kitchen City - Taft</span>
+      </a>
+      <div class="dropdown-menu dropdown-menu-small">
+        <a class="dropdown-item" href="user-profile-lite.html">
+          <i class="material-icons">&#xE7FD;</i> Profile</a>
+        <a class="dropdown-item" href="components-blog-posts.html">
+          <i class="material-icons">vertical_split</i>Change Password</a>
+        <a class="dropdown-item" href="add-new-post.html">
+          <i class="material-icons">note_add</i>Preferences</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item text-danger" href="#">
+          <i class="material-icons text-danger">&#xE879;</i> Logout </a>
+      </div>
+    </li>
+  </ul>
+  <nav class="nav">
+    <a href="#" class="nav-link nav-link-icon toggle-sidebar d-md-inline d-lg-none text-center border-left" data-toggle="collapse" data-target=".header-navbar" aria-expanded="false" aria-controls="header-navbar">
+      <i class="material-icons">&#xE5D2;</i>
+    </a>
+  </nav>
+</nav>
