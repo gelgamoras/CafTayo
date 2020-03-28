b76102b540691976c5d313ec7b8537ac00a72bdf
jellily6 <gelgamoras@gmail.com>
2020-03-29 04:01:44 +0800

    admin routes

--- routes/web.php
+++ routes/web.php
@@ -20,22 +20,26 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 //======== CONCESSIONAIRE ========
-//----MENU
-Route::get('/concessionaire', 'MenuController@showToday')->name('con_homepage'); 
-Route::get('/concessionaire/menu/create', 'MenuController@create')->name('menu_create'); 
-Route::get('/concessionaire/menu', 'MenuController@index')->name('menu_list'); 
-Route::get('/concessionaire/menu/1', 'MenuController@show')->name('menu_detail'); 
-Route::get('/concessionaire/menu/everyday', 'MenuController@showEveryday')->name('menu_everyday'); 
-//----FOOD
-Route::get('/concessionaire/food/create', 'FoodController@create')->name('food_create');
-Route::get('/concessionaire/food', 'FoodController@index')->name('food_list');  
-Route::get('/concessionaire/food/1', 'FoodController@show')->name('food_detail'); 
-//----SETTINGS (Period)
-Route::get('/concessionaire/settings', 'PeriodController@index')->name('con_settings');
+    //----MENU
+        Route::get('/concessionaire', 'MenuController@showToday')->name('con_homepage'); 
+        Route::get('/concessionaire/menu/create', 'MenuController@create')->name('menu_create'); 
+        Route::get('/concessionaire/menu', 'MenuController@index')->name('menu_list'); 
+        Route::get('/concessionaire/menu/1', 'MenuController@show')->name('menu_detail'); 
+        Route::get('/concessionaire/menu/everyday', 'MenuController@showEveryday')->name('menu_everyday'); 
+    //----FOOD
+        Route::get('/concessionaire/food/create', 'FoodController@create')->name('food_create');
+        Route::get('/concessionaire/food', 'FoodController@index')->name('food_list');  
+        Route::get('/concessionaire/food/1', 'FoodController@show')->name('food_detail'); 
+    //----SUBCATEGORY 
+        Route::get('/concessionaire/subcategories', 'MenuController@create_subcategory')->name('subcategory_create'); 
+    //----SETTINGS (Period)
+        Route::get('/concessionaire/settings', 'PeriodController@index')->name('con_settings');
 
 //======== ADMINISTRATOR ========
-Route::get('/administrator', function(){return view('administrator.administrator');})->name('admin_homepage');
-//----USERS
-Route::get("/administrator/users", 'UserController@index')->name('users_list'); 
-Route::get("/administrator/users/view", 'UserController@show')->name('users_detail'); 
-Route::get('/administrator/users/create', 'UserController@create')->name('users_create'); 
+    Route::get('/administrator', function(){return view('administrator.administrator');})->name('admin_homepage');
+    //----USERS
+        Route::get("/administrator/users", 'UserController@index')->name('users_list'); 
+        Route::get("/administrator/users/view", 'UserController@show')->name('users_detail'); 
+        Route::get('/administrator/users/create', 'UserController@create')->name('users_create'); 
+    //----CAMPUS
+        Route::get("/administrator/campus", 'CampusController@index')->name('campus_list'); 
