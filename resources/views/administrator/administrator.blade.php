9bd96729dfa098050582580bc216c3c88ab2bb8a
jellily6 <gelgamoras@gmail.com>
2020-03-29 15:30:21 +0800

    added content to admin homepage

--- resources/views/administrator/administrator.blade.php
+++ resources/views/administrator/administrator.blade.php
@@ -13,5 +13,130 @@
 @endsection
 
 @section('content')
-
+<div class="row">
+    <div class="col-lg col-md-6 col-sm-6 mb-4">
+        <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+                <div class="d-flex flex-column m-auto">
+                    <div class="stats-small__data text-center">
+                    <span class="stats-small__label text-uppercase">Subscribers</span>
+                    <h6 class="stats-small__value count my-3">2,390</h6>
+                    </div>
+                    <div class="stats-small__data">
+                    <span class="stats-small__percentage stats-small__percentage--increase">4.7%</span>
+                    </div>
+                </div>
+                <canvas height="120" class="blog-overview-stats-small-1"></canvas>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg col-md-6 col-sm-6 mb-4">
+        <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+                <div class="d-flex flex-column m-auto">
+                    <div class="stats-small__data text-center">
+                        <span class="stats-small__label text-uppercase">Food</span>
+                        <h6 class="stats-small__value count my-3">182</h6>
+                    </div>
+                    <div class="stats-small__data">
+                        <span class="stats-small__percentage stats-small__percentage--increase">12.4%</span>
+                    </div>
+                </div>
+                <canvas height="120" class="blog-overview-stats-small-2"></canvas>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg col-md-6 col-sm-6 mb-4">
+        <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+                <div class="d-flex flex-column m-auto">
+                    <div class="stats-small__data text-center">
+                        <span class="stats-small__label text-uppercase">Menu</span>
+                        <h6 class="stats-small__value count my-3">182</h6>
+                    </div>
+                    <div class="stats-small__data">
+                        <span class="stats-small__percentage stats-small__percentage--increase">12.4%</span>
+                    </div>
+                </div>
+                <canvas height="120" class="blog-overview-stats-small-4"></canvas>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg col-md-4 col-sm-6 mb-4">
+        <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+                <div class="d-flex flex-column m-auto">
+                    <div class="stats-small__data text-center">
+                        <span class="stats-small__label text-uppercase">Users</span>
+                        <h6 class="stats-small__value count my-3">8,147</h6>
+                    </div>
+                    <div class="stats-small__data">
+                        <span class="stats-small__percentage stats-small__percentage--decrease">3.8%</span>
+                    </div>
+                </div>
+                <canvas height="120" class="blog-overview-stats-small-3"></canvas>
+            </div>
+        </div>
+    </div>
+</div> 
+<div class="row">
+    <div class="col-lg-3 col-md-12 col-sm-12 mb-4">
+        <div class="card card-small">
+            <div class="card-header border-bottom">
+            <h6 class="m-0">Top Referrals</h6>
+            </div>
+            <div class="card-body p-0">
+            <ul class="list-group list-group-small list-group-flush">
+                <li class="list-group-item d-flex px-3">
+                <span class="text-semibold text-fiord-blue">GitHub</span>
+                <span class="ml-auto text-right text-semibold text-reagent-gray">19,291</span>
+                </li>
+                <li class="list-group-item d-flex px-3">
+                <span class="text-semibold text-fiord-blue">Stack Overflow</span>
+                <span class="ml-auto text-right text-semibold text-reagent-gray">11,201</span>
+                </li>
+                <li class="list-group-item d-flex px-3">
+                <span class="text-semibold text-fiord-blue">Hacker News</span>
+                <span class="ml-auto text-right text-semibold text-reagent-gray">9,291</span>
+                </li>
+                <li class="list-group-item d-flex px-3">
+                <span class="text-semibold text-fiord-blue">Reddit</span>
+                <span class="ml-auto text-right text-semibold text-reagent-gray">8,281</span>
+                </li>
+                <li class="list-group-item d-flex px-3">
+                <span class="text-semibold text-fiord-blue">The Next Web</span>
+                <span class="ml-auto text-right text-semibold text-reagent-gray">7,128</span>
+                </li>
+                <li class="list-group-item d-flex px-3">
+                <span class="text-semibold text-fiord-blue">Tech Crunch</span>
+                <span class="ml-auto text-right text-semibold text-reagent-gray">6,218</span>
+                </li>
+                <li class="list-group-item d-flex px-3">
+                <span class="text-semibold text-fiord-blue">YouTube</span>
+                <span class="ml-auto text-right text-semibold text-reagent-gray">1,218</span>
+                </li>
+                <li class="list-group-item d-flex px-3">
+                <span class="text-semibold text-fiord-blue">Adobe</span>
+                <span class="ml-auto text-right text-semibold text-reagent-gray">827</span>
+                </li>
+            </ul>
+            </div>
+            <div class="card-footer border-top">
+            <div class="row">
+                <div class="col">
+                <select class="custom-select custom-select-sm">
+                    <option selected>Last Week</option>
+                    <option value="1">Today</option>
+                    <option value="2">Last Month</option>
+                    <option value="3">Last Year</option>
+                </select>
+                </div>
+                <div class="col text-right view-report">
+                <a href="#">Full report &rarr;</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div> 
 @endsection
