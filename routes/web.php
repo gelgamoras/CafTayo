ed5dfd74ca5fe9e17865b9c46b5451f56eade87d
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-04-04 18:04:27 +0800

    Updated web routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Auth::routes();
+Route::get('/', function () { return view('welcome'); });
+
+Route::prefix('user')->group(function () {
+    Auth::routes();
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
