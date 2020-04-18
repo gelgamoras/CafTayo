ece67b86731804aee564d3c1ec7f1f48b52c69b3
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-04-18 15:38:07 +0800

    Added isAdmin middleware

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request->user() && $request->user()->role != 'Admin')
+        {
+            return redirect(RouteServiceProvider::HOME);
+            //return new Response(view(‘unauthorized’)->with(‘role’, ‘ADMIN’));
+        }
+        return $next($request);
+    }
+}
