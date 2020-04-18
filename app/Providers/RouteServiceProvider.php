2cb64495cf66ef94beb7932017ad6108abf0445d
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-04-18 15:38:24 +0800

    Added dashboard controller

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/user/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -30,8 +30,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-
         parent::boot();
     }
 
@@ -43,10 +41,7 @@ class RouteServiceProvider extends ServiceProvider
     public function map()
     {
         $this->mapApiRoutes();
-
         $this->mapWebRoutes();
-
-        //
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -15,33 +15,46 @@
 Route::get('/', function () { return view('welcome'); });
 
 Route::prefix('user')->group(function () {
+    
+    // Auth Routes
     Auth::routes();
+
+    // Dashboard
+    Route::get('/', 'DashboardController@index')->name('dashboard.index');
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::middleware('auth')->group(function() { 
-    Route::prefix('campus')->group(function () {
-        Route::get('/', 'CampusController@index')->name('campus.index');
-        Route::get('/create', 'CampusController@create')->name('campus.create');
-        Route::post('/create', 'CampusController@store')->name('campus.store');
-        Route::get('/{id}', 'CampusController@show')->name('campus.show');
-        Route::get('/{id}/edit', 'CampusController@edit')->name('campus.edit');
-        Route::put('/{id}', 'CampusController@update')->name('campus.update');
-        Route::delete('/index/{id}', 'CampusController@destroy')->name('campus.destroy');
+
+    // For Admins Only
+    Route::middleware('isAdmin')->group(function() {
+        
+        // Campus
+        Route::prefix('campus')->group(function () {
+            Route::get('/', 'CampusController@index')->name('campus.index');
+            Route::get('/create', 'CampusController@create')->name('campus.create');
+            Route::post('/create', 'CampusController@store')->name('campus.store');
+            Route::get('/{id}', 'CampusController@show')->name('campus.show');
+            Route::get('/{id}/edit', 'CampusController@edit')->name('campus.edit');
+            Route::put('/{id}', 'CampusController@update')->name('campus.update');
+            Route::delete('/index/{id}', 'CampusController@destroy')->name('campus.destroy');
+        });
+
+        // Category
+        Route::prefix('category')->group(function() {
+            Route::get('/', 'CategoriesController@index')->name('categories.index');
+            Route::get('/create', 'CategoriesController@create')->name('categories.create');
+            Route::post('/create', 'CategoriesController@store')->name('categories.store');
+            Route::get('/{id}', 'CategoriesController@show')->name('categories.show');
+            Route::get('/{id}/edit', 'CategoriesController@edit')->name('categories.edit');
+            Route::put('/{id}', 'CategoriesController@update')->name('categories.update');
+            Route::delete('/{id}', 'CategoriesController@destroy')->name('categories.destroy');
+        });
+
+        // Period
+
     });
 });
 
-
-/*
-//categories by ELLAAAAA
-Route::get('/categories/index', 'CategoriesController@index')->name('categories.index');
-Route::get('/categories/create', 'CategoriesController@create')->name('categories.create');
-Route::post('/categories/create', 'CategoriesController@store')->name('categories.store');
-Route::get('categories/index/{id}', 'CategoriesController@show')->name('categories.show');
-Route::get('categories/index/{id}/edit', 'CategoriesController@edit')->name('categories.edit');
-Route::put('categories/index/{id}', 'CategoriesController@update')->name('categories.update');
-Route::delete('categories/index/{id}', 'CategoriesController@destroy')->name('categories.destroy');
-*/
+//Categories Food, Menu, MenuItem, Period, Subscribers, User
 
 
