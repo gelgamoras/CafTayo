97d7d1ca5085b40d32bb70f2e8ad9accf0f46d49
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-04-22 19:53:42 +0800

    Update app template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -75,6 +72,10 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        @yield('scripts')
     </div>
 </body>
 </html>
