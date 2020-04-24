d05cede329bbe62ce109dd1c5f0faafc17817cb1
jellily6 <gelgamoras@gmail.com>
2020-04-24 09:11:41 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header border-bottom"><h4 class="mb-0">Dashboard</h4></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,8 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <span style="font-weight: 300">You are logged in!</span>
 
-                    You are logged in!
                 </div>
             </div>
         </div>
