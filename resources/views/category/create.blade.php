ec9c8078609458c731e1542361c654d7ca3b8044
gel uwu <gelgamoras@gmail.com>
2020-04-28 02:53:55 +0800

    added ui to create and edit category

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,36 +1,61 @@
-<form method="POST" action="{{ route('categories.store', request()->route('campus')) }}">
-    @csrf
-    <div class="form-row">
-        <div class="form-group w-100 px-1">
-            <label for="name">{{ __('Category Name') }}</label>
-            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Name of Category" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-            @error('name')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div> 
-    </div> 
-    <div class="form-row">
-        <div class="form-group w-100 px-1">
-            <label for="parent">{{ __('Parent') }}</label>
+@extends('layouts.dashboard.main')
+
+
+@section('subheader')
+<a href="javascript:history.back()" class="btn btn-secondary btn-sm mb-2" style="text-transform: none; letter-spacing: initial;">
+    Back
+</a>
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-lg-6">
+        <div class="card card-small mb-4">
+            <div class="card-header border-bottom">
+                <h5 class="mb-0">Create a Category</h5> 
+            </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('categories.store', request()->route('campus')) }}">
+                    @csrf
+                    <div class="form-row">
+                        <div class="form-group col">
+                            <label for="name">{{ __('Category Name') }}</label>
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Name of Category" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col">
+                            <label for="parent">{{ __('Parent') }}</label>
            
-            <select id="parent" name="parent" class="form-control @error('parent') is-invalid @enderror" required>
-                <option value="0" selected>Parent (None)</option>
-                @foreach($index as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
-                @endforeach
-              </select>
-        
-            @error('parent')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div> 
-    </div> 
-    <div class="form-row px-1">
-        <button type="submit" class="btn btn-sm btn-primary mt-3">Add Category</button> 
-    </div> 
-</form> 
+                            <select id="parent" name="parent" class="form-control @error('parent') is-invalid @enderror" required>
+                                <option value="0" selected>Parent (None)</option>
+                                @foreach($index as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+                                </select>
+                        
+                            @error('parent')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col form-group">
+                            <button type="submit" class="btn btn-sm btn-primary mt-3">Add Category</button> 
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,40 +1,64 @@
-@if($errors->any())
-    {{ implode('', $errors->all('<div>:message</div>')) }}
-@endif
 
-<form method="POST" action="{{ route('categories.store', request()->route('campus')) }}">
-    @csrf
-    <div class="form-row">
-        <div class="form-group w-100 px-1">
-            <label for="name">{{ __('Category Name') }}</label>
-            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Name of Category" name="name" value="{{ $category->name }}" required autocomplete="name" autofocus>
+@extends('layouts.dashboard.main')
 
-            @error('name')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div> 
-    </div> 
-    <div class="form-row">
-        <div class="form-group w-100 px-1">
-            <label for="parent">{{ __('Parent') }}</label>
+
+@section('subheader')
+<a href="javascript:history.back()" class="btn btn-secondary btn-sm mb-2" style="text-transform: none; letter-spacing: initial;">
+    Back
+</a>
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-lg-6">
+        @if($errors->any())
+            {{ implode('', $errors->all('<div>:message</div>')) }}
+        @endif
+        <div class="card card-small mb-4">
+            <div class="card-header border-bottom">
+                <h5 class="mb-0">Update a Category</h5> 
+            </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('categories.store', request()->route('campus')) }}">
+                    @csrf
+                    <div class="form-row">
+                        <div class="form-group col">
+                            <label for="name">{{ __('Category Name') }}</label>
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Name of Category" name="name" value="{{ $category->name }}" required autocomplete="name" autofocus>
+
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col">
+                            <label for="parent">{{ __('Parent') }}</label>
            
-            <select id="parent" name="parent" class="form-control @error('parent') is-invalid @enderror" required>
-                <option value="0" @if($category->parent_id == null || $category->parent_id == 0) selected @endif>Parent (None)</option>
-                @foreach($index as $cat)
-                    <option value="{{ $cat->id }}" @if($category->parent_id == $cat->id) selected @endif>{{ $cat->name }}</option>
-                @endforeach
-              </select>
-        
-            @error('parent')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div> 
-    </div> 
-    <div class="form-row px-1">
-        <button type="submit" class="btn btn-sm btn-primary mt-3">Update Category</button> 
-    </div> 
-</form> 
+                            <select id="parent" name="parent" class="form-control @error('parent') is-invalid @enderror" required>
+                                <option value="0" @if($category->parent_id == null || $category->parent_id == 0) selected @endif>Parent (None)</option>
+                                @foreach($index as $cat)
+                                    <option value="{{ $cat->id }}" @if($category->parent_id == $cat->id) selected @endif>{{ $cat->name }}</option>
+                                @endforeach
+                                </select>
+                        
+                            @error('parent')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col form-group">
+                            <button type="submit" class="btn btn-sm btn-primary mt-3">Update Category</button> 
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
