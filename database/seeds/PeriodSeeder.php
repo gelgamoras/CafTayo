ecfffd2d2f6377978e92d56f54d61fdc70b131fe
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-04-28 21:09:01 +0800

    Added period seeder

---
+++ database/seeds/PeriodSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PeriodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('periods')->insert([
+            'period' => 'Breakfast',
+            'start' => '06:00',
+            'end' => '10:00',
+            'status' => 'active'
+        ]);
+
+        DB::table('periods')->insert([
+            'period' => 'Lunch',
+            'start' => '10:00',
+            'end' => '14:00',
+            'status' => 'active'
+        ]);
+        
+        DB::table('periods')->insert([
+            'period' => 'Breakfast',
+            'start' => '14:00',
+            'end' => '16:00',
+            'status' => 'active'
+        ]);
+
+        DB::table('periods')->insert([
+            'period' => 'Dinner',
+            'start' => '17:00',
+            'end' => '21:00',
+            'status' => 'active'
+        ]);
+    }
+}
