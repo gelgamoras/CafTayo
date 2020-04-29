a4a339e1f9ca0bd1c3f155ef021d6bd48fb83337
gel uwu <gelgamoras@gmail.com>
2020-04-30 06:35:46 +0800

    changed time so it wont overlap

--- database/seeds/PeriodSeeder.php
+++ database/seeds/PeriodSeeder.php
@@ -15,27 +15,27 @@ class PeriodSeeder extends Seeder
         DB::table('periods')->insert([
             'period' => 'Breakfast',
             'start' => '06:00',
-            'end' => '10:00',
+            'end' => '09:59',
             'status' => 'active'
         ]);
 
         DB::table('periods')->insert([
             'period' => 'Lunch',
             'start' => '10:00',
-            'end' => '14:00',
+            'end' => '13:59',
             'status' => 'active'
         ]);
         
         DB::table('periods')->insert([
             'period' => 'Snacks',
             'start' => '14:00',
-            'end' => '16:00',
+            'end' => '17:59',
             'status' => 'active'
         ]);
 
         DB::table('periods')->insert([
             'period' => 'Dinner',
-            'start' => '17:00',
+            'start' => '18:00',
             'end' => '21:00',
             'status' => 'active'
         ]);
